d519d6a8d37d53fe5b136359a654232051368674
prisma arya pandhega <prisma.arya@bukalapak.com>
2019-08-10 00:19:19 +0700

    Push pertama

---
+++ APITesting/Test1.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'httparty'
+
+# class EdutechionalResty
+#   include HTTParty
+#   base_uri "http://edutechional-resty.herokuapp.com"
+
+#   def posts
+#     self.class.get('/posts.json')
+#   end
+# end
+
+# api = EdutechionalResty.new
+# puts api.posts
+
+response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
+
+puts response.message
