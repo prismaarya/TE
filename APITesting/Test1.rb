049f39f0aeeed39c7299ee4b70ccc202bdfd6450
prisma arya pandhega <prisma.arya@bukalapak.com>
2019-08-14 18:08:12 +0700

    sd

--- APITesting/Test1.rb
+++ APITesting/Test1.rb
@@ -1,17 +1,17 @@
 require 'rubygems'
 require 'httparty'
 
-# class EdutechionalResty
-#   include HTTParty
-#   base_uri "http://edutechional-resty.herokuapp.com"
+ class EdutechionalResty
+   include HTTParty
+   base_uri "http://edutechional-resty.herokuapp.com"
 
-#   def posts
-#     self.class.get('/posts.json')
-#   end
-# end
+   def posts
+     self.class.get('/posts.json')
+   end
+ end
 
-# api = EdutechionalResty.new
-# puts api.posts
+ api = EdutechionalResty.new
+ puts api.posts
 
 response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
 
