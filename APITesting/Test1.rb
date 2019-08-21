48a8b4ae08e18679b7070b46d7b0217e022136fd
prisma arya pandhega <prisma.arya@bukalapak.com>
2019-08-21 09:33:18 +0700

    branch conflict

--- APITesting/Test1.rb
+++ APITesting/Test1.rb
@@ -16,5 +16,5 @@ require 'httparty'
 response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
 
 puts response.body
-puts response.haii
+puts response.branchconflict
 puts response.status
